29f6c4d7fa44b45fade9d2226012f1c59f002640
Sol <soltalebali@gmail.com>
2026-06-29 17:39:47 +0100

    Fix FraudRule interface and rename typo in test file
    - FraudRule: add missing evaluate method and imports (was committed empty)
    - Rename UnusualLocationRileTest -> UnusualLocationRuleTest
    - Minor: trailing newline fixes in HighAmountRule files

--- src/main/java/io/github/soltalebali/frauddetection/rules/FraudRule.java
+++ src/main/java/io/github/soltalebali/frauddetection/rules/FraudRule.java
@@ -1,4 +1,8 @@
 package io.github.soltalebali.frauddetection.rules;
 
+import io.github.soltalebali.frauddetection.model.FlagResult;
+import io.github.soltalebali.frauddetection.model.Transaction;
+
 public interface FraudRule {
+    FlagResult evaluate(Transaction transaction);
 }
